a1ff244e2262e08c61dfdd062f5be6bcce010c74
Lali <lauradiaz1586@gmail.com>
2021-02-15 16:20:56 +0400

    initial set up, modify readme

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './components/Loading';
+import Tours from './components/Tours';
+import './App.css';
+
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  return <h2>tour project</h2>;
+}
+
+export default App;
