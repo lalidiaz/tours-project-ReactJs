5b4e7cefdc96f05e5f763f9501a17a87e326537d
Lali <lauradiaz1586@gmail.com>
2021-02-15 16:53:34 +0400

    add toggle to read more, fetch data about tours from api, work on remove tour button

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is the 2nd project part of an artcile I am writing to refresh ReactJs concepts and share it with the community once it's finished.
 
-Concept: useEffect() and Conditional Rendering.
+Concepts: useEffect() and Conditional Rendering.
--- src/App.js
+++ src/App.js
@@ -6,7 +6,45 @@ import './App.css';
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  return <h2>tour project</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
 export default App;
